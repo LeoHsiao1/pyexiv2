acf7e6a74b8cc6790edc46074084b184ae09bea4
LeoHsiao <leohsiao@foxmail.com>
2019-06-24 20:14:41 +0800

    wrote a function to encode unicode string

--- core/use_dll.py
+++ core/use_dll.py
@@ -2,22 +2,22 @@ import ctypes
 import json
 import os
 
-# if the filename contains Chinese characters:
-filename_encode = "gbk"
-# else:
-# filename_encode = "ascii"
-
 
 dll_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "lib")
 ctypes.CDLL(os.path.join(dll_dir, "exiv2.dll"))   # import it at first
 api = ctypes.CDLL(os.path.join(dll_dir, "api.dll"))
 
 
+def encode(s):
+    """ Unicode characters need to be handled, because bytes can only contain ASCII literal characters. """
+    return s.encode("gbk")  # or "ascii"
+
+
 def read_exif(filename):
     """ return a dict """
 
     api.exif.restype = ctypes.c_char_p
-    buffer = api.exif(filename.encode(filename_encode))
+    buffer = api.exif(encode(filename))
     # api.free_buffer()     # C++ program can free the buffer automatically
 
     _buffer = buffer.decode()
