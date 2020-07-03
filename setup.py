4518acecc57f7e900713074898b2ac648ed91922
LeoHsiao <leohsiao@foxmail.com>
2020-07-03 23:09:24 +0800

    release v2.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='pyexiv2',
-    version='2.3.0',
+    version='2.3.1',
     author='LeoHsiao',
     author_email='leohsiao@foxmail.com',
     description='Read/Write metadata of digital image, including EXIF, IPTC, XMP.',
