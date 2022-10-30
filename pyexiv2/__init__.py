ab3759fd14964bef7d3b8b212be1895b34ab4b2c
LeoHsiao <leohsiao@foxmail.com>
2022-10-30 14:58:19 +0800

    [CI] ready to release v2.8.1

--- pyexiv2/__init__.py
+++ pyexiv2/__init__.py
@@ -6,7 +6,7 @@ Read/Write metadata(including EXIF, IPTC, XMP), comment and ICC Profile embedded
 from .core import *
 
 
-__version__ = '2.8.0'
+__version__ = '2.8.1'
 __exiv2_version__ = exiv2api.version()
 
 
