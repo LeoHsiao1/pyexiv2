0a5d9ef5a5ad07ee4b758b7d5ddc4a763510c258
LeoHsiao <leohsiao@foxmail.com>
2025-05-07 10:32:54 +0000

    [test] update show_version.py

--- pyexiv2/tests/show_version.py
+++ pyexiv2/tests/show_version.py
@@ -1,7 +1,10 @@
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 import platform
 print(platform.uname())
 
 import pyexiv2
-print('__path__=', pyexiv2.__path__)
-print('__version__=', pyexiv2.__version__)
-print('__exiv2_version__=', pyexiv2.__exiv2_version__)
+print('__path__={}'.format(pyexiv2.__path__))
+print('__version__={}'.format(pyexiv2.__version__))
+print('__exiv2_version__={}'.format(pyexiv2.__exiv2_version__))
