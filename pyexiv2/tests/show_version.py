8ffd9e0da8b0ac6b8daea11feb00b5af83e59725
LeoHsiao <leohsiao@foxmail.com>
2025-05-07 09:54:19 +0000

    [test] add show_version.py

---
+++ pyexiv2/tests/show_version.py
@@ -0,0 +1,7 @@
+import platform
+print(platform.uname())
+
+import pyexiv2
+print('__path__=', pyexiv2.__path__)
+print('__version__=', pyexiv2.__version__)
+print('__exiv2_version__=', pyexiv2.__exiv2_version__)
